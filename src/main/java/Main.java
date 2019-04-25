19d12f43d05f88a0c87a3a5447c5d279cf0ef7b7
cbudtz <chrbudtz@gmail.com>
2019-04-25 18:01:34 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.BsonDocument;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.jongo.Jongo;
+
+import static com.mongodb.client.model.Filters.eq;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Create Client for MongoDB
+        MongoClient client = new MongoClient();
+        //Map Java DTO's to JSON
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        //Fetch database with Mapper
+        MongoDatabase database = client.getDatabase("javaMongoDB").withCodecRegistry(pojoCodecRegistry);
+
+        MongoCollection<Person> personCollection = database.getCollection("persons", Person.class);
+        Person brian = new Person();
+        brian.setName("brian");
+        personCollection.insertOne(brian);
+        System.out.println("Person inserted");
+
+        FindIterable<Person> people = personCollection.find(eq("name", "brian"));
+        Person retrievedPerson = people.first();
+        System.out.println("Found: " + (retrievedPerson != null ? retrievedPerson.getName() : null));
+
+
+    }
+}
